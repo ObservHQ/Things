7e57fa2fee7d1cbefb1751ed22c6d7b57bb4a5d3
Anil Seervi <61609033+AnilSeervi@users.noreply.github.com>
2024-01-06 22:12:13 +0530

    fix highlighting (#2)
    * fix highlighting
    
    * remove unused fonts
    
    * update domain

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,13 @@
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import solidJs from "@astrojs/solid-js"
-import vercel from '@astrojs/vercel/serverless'
-import vue from "@astrojs/vue"
-import { defineConfig } from "astro/config"
-import unocss from "unocss/astro"
-import vesper from "./src/lib/vesper.json"
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
+import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
 
 export default defineConfig({
-  site: "https://www.observhq.com",
+  site: "https://observhq.vercel.app",
   adapter: vercel(),
   output: "hybrid",
   integrations: [
@@ -18,7 +17,8 @@ export default defineConfig({
     unocss({ injectReset: true }),
     vue(),
   ],
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  markdown: { shikiConfig: { theme: vesper, wrap: true } },
-})
+  server: {
+    port: 6969,
+  },
+  markdown: { shikiConfig: { theme: "vitesse-dark", wrap: true } },
+});
